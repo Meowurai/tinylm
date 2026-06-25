3934050f2ad0c7b31b345100f49c39ea083dd1f2
Caj Michael Andersson <caj.michael.dev@gmail.com>
2026-06-25 05:13:22 +0200

    add test for datasetbuilder and tokenizer together

--- tests/test_dataset_builder.py
+++ tests/test_dataset_builder.py
@@ -1,5 +1,5 @@
-import pytest 
 from tinylm.dataset_builder import build_examples
+from tinylm.tokenizer import Tokenizer
 
 def test_build_examples_from_hello():
     examples = build_examples(
@@ -15,3 +15,17 @@ def test_build_examples_from_hello():
         ([0, 2, 1, 3], 3),
         ([2, 1, 3, 3], 4),
     ]
+
+def test_tokenizer_and_datasetbuilder():
+    tokenizer = Tokenizer.from_text("hello")
+    ids = tokenizer.encode("hello")
+
+    examples = build_examples(
+        ids=ids,
+        start_id=tokenizer.token_to_id["<START>"],
+        context_size=4,
+    )
+
+    assert examples[0] == ([0, 0, 0, 0], 2)
+    assert examples[-1] == ([2, 1, 3, 3], 4)
+
